4a79930d91c544dca3ad305c9ddb34c22cfa3263
CHAHAT SAINI <chahatsaini1309@gmail.com>
2026-08-06 15:03:54 +0530

    Time: 4 ms (13.54%), Space: 16.6 MB (86.77%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int x[4] = {-1, 1, 0, 0};
+    int y[4] = {0, 0, -1, 1};
+
+    bool isvalid(int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return false;
+        }
+        return true;
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int time = 0;
+        int fresh = 0;
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                    grid[i][j] = -2;
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+
+        while(!q.empty() && fresh > 0){
+            time++;
+            int s = q.size();
+
+            while(s--){
+                pair<int, int> p = q.front();
+                q.pop();
+                
+                int r = p.first;
+                int c = p.second;
+
+                for(int k = 0; k < 4; k++){
+                    int row = r + x[k];
+                    int col = c + y[k];
+
+                    if(isvalid(row, col, m, n) && grid[row][col] == 1){
+                        q.push({row,col});
+                        grid[row][col] = -2;
+                        fresh--;
+                    }
+                }
+            }
+        }
+
+        if(fresh > 0){
+            return -1;
+        }
+
+        return time;
+
+    }
+};
